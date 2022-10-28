129b8aedd014c71eda128a97af0141996122ddd9
Corina <morarik8@gmail.com>
2022-10-28 13:27:13 +0300

    add function Number Validation

--- script/script.js
+++ script/script.js
@@ -1,5 +1,8 @@
 const email = document.getElementById("email");
-const btn_sign_up = document.getElementById("btn-sign-up");
+const btn_sign_up = document.querySelector("#btn-sign-up");
+const phone_number = document.querySelector('#phone');
+
+
 let users = [];
 btn_sign_up.addEventListener("click", () => {
   if (users.includes(email.value)) {
@@ -9,8 +12,28 @@ btn_sign_up.addEventListener("click", () => {
     email.value.match("@yahoo.com") ||
     email.value.match("@icloud.com")
   ) {
-    users.push(email.value);
-  } else {
-    alert("Not Enough Rights to Register here");
-  }
+    users.push(email.value);}
+  // } else {
+  //   alert("Not Enough Rights to Register here");
+  // }
 });
+
+const numberValidation = () => {
+    const number = phone_number.value;
+
+    if(number === "" || number.length !== 12
+        || /[a-zA-Z]/.test(number) || number.startsWith('+373') === false
+        || number.charCodeAt(4) < 54 || number.charCodeAt(4) > 55 )
+    {
+        phone_number.style.borderColor = "red"
+        phone_number.style.color = "red"
+        phone_number.value = "Invalid number!"
+        return false;
+    }
+    else
+    {
+        phone_number.style.color = "black";
+        return true
+    }
+}
+
